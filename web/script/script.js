5af5adc7b24ef03310f159e74de3064f798e6370
fiorz <68775947+AlexGeeBee@users.noreply.github.com>
2025-12-09 22:32:39 +0300

    fixed burger menu, booking page, added FAQ

---
+++ web/script/script.js
@@ -0,0 +1,36 @@
+let questions = document.querySelectorAll('.question');
+
+questions.forEach((question) => {
+    question.addEventListener('click', () => {
+
+        let answer = question.lastElementChild;
+
+        if (answer.style.maxHeight) {
+            document.querySelectorAll(".answer").forEach((el) => el.style.maxHeight = null);
+            answer.style.marginTop = null;
+            answer.previousElementSibling.style.transform = null;
+        }
+        else {
+            document.querySelectorAll(".answer").forEach((el) => {
+                el.style.maxHeight = null;
+                el.style.marginTop = null;
+            });
+            document.querySelectorAll(".question__open").forEach((el) => el.style.transform = null);
+            
+            answer.style.maxHeight = answer.scrollHeight + "px";
+            answer.style.marginTop = "30px";
+            answer.previousElementSibling.style.transform = "rotate(45deg)";
+        }
+
+    });
+})
+
+
+document.querySelector(".burger-checkbox").addEventListener("change", (e) => {
+    if (e.target.checked) {
+        document.body.style.overflow = 'hidden';
+    }
+    else {
+        document.body.style.overflow = null;
+    }
+});
